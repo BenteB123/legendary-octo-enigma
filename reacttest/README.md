b029ed473a647f5a9c30109613a3dd31c18b5e7a
Erik <eriklingaberntsen@gmail.com>
2024-09-26 23:11:03 +0200

    feat(reacttest): added routing and simple layout

--- reacttest/README.md
+++ reacttest/README.md
@@ -6,11 +6,11 @@ This is a React project built with Vite and TypeScript.
 
 1. Clone the repository
 2. Install dependencies: `npm install`
-3. Start the development server: `npm run dev`
+3. Start the development server: `npm run start`
 
 ## Available Scripts
 
-- `npm run dev`: Start the development server
+- `npm run start`: Start the development server
 - `npm run build`: Build the project for production
 - `npm run preview`: Preview the production build locally
 
--- reacttest/src/App.tsx
+++ reacttest/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
+import { Routes, Route, Link, Outlet } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function Home() {
   const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
+      <h2>Home</h2>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Project Legendary Octo Enigma</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,7 +29,48 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
+  )
+}
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+      <Outlet />
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is the about page.</p>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" Component={Layout} >
+          <Route index Component={Home} />
+          <Route path="about" Component={About} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
 
